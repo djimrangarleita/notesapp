f0a35ee93d38db234594ac086de3914ac4e5efcf
djimrangarleita <dngarleita@gmail.com>
2022-06-13 13:12:51 +0100

    Error handling and generic alert dialog

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/const/routes.dart';
+
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -69,11 +70,22 @@ class _LoginViewState extends State<LoginView> {
                   (route) => false,
                 );
               } on FirebaseAuthException catch (e) {
-                if (e.code == 'user-not-found') {
-                  devtools.log("User not found");
+                if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+                  await showErrorDialog(
+                    context,
+                    "Invalid credentials",
+                  );
                 } else {
-                  devtools.log('Something wrong happened');
+                  await showErrorDialog(
+                    context,
+                    "Error: ${e.code}",
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text('Login'),
